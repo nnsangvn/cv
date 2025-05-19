0e68da8247d2020df676281473dbc6ea85bfa26b
Nguyễn Nhất Sang <nguyennhatsang2002@gmail.com>
2025-05-19 23:58:35 +0700

    optimize image

--- src/components/magicui/stack.tsx
+++ src/components/magicui/stack.tsx
@@ -1,5 +1,6 @@
 import { motion, useMotionValue, useTransform } from "framer-motion";
 import { useState } from "react";
+import Image from 'next/image';
 
 interface CardRotateProps {
   children: React.ReactNode;
@@ -64,7 +65,8 @@ export default function Stack({
         { id: 1, img: "https://images.unsplash.com/photo-1480074568708-e7b720bb3f09?q=80&w=500&auto=format" },
         { id: 2, img: "https://images.unsplash.com/photo-1449844908441-8829872d2607?q=80&w=500&auto=format" },
         { id: 3, img: "https://images.unsplash.com/photo-1452626212852-811d58933cae?q=80&w=500&auto=format" },
-        { id: 4, img: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?q=80&w=500&auto=format" }
+        { id: 4, img: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?q=80&w=500&auto=format" },
+        { id: 5, img: "https://images.unsplash.com/photo-1572120360610-d971b9d7767c?q=80&w=500&auto=format" }
       ]
   );
 
@@ -117,11 +119,11 @@ export default function Stack({
                 height: cardDimensions.height,
               }}
             >
-              <img
-                src={card.img}
-                alt={`card-${card.id}`}
-                className="w-full h-full object-cover pointer-events-none"
-              />
+            <Image
+              src={card.img}
+              alt={`card-${card.id}`}
+              className="w-full h-full object-cover pointer-events-none"
+            />
             </motion.div>
           </CardRotate>
         );
